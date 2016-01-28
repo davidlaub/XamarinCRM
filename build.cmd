e8241e642ace355fdda44d8028f3586417a9ff9b
nikhil-neu <nikhil.rajan67@gmail.com>
2016-01-28 21:12:24 +0530

    adding the updated build.cmd

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@
 cls
 cls
 "tools\NuGet.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-packages\FAKE\tools\FAKE.exe build.fsx %* 2>&1
+tools\FAKE\tools\FAKE.exe build.fsx %* 2>&1
